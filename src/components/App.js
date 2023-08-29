368d8160f278522d59908fb1c0ce1972491944e1
VipulTrimbake610 <vipultrimbake610@gmail.com>
2023-08-29 23:16:05 +0530

    submitting code-Tue Aug 29 2023 23:16:01 GMT+0530 (India Standard Time)

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,22 @@
 
-import React from "react";
+import React, { useEffect, useState } from "react";
 import './../styles/App.css';
+// import { data } from "cypress/types/jquery";
+
 
 const App = () => {
+  let [arr,setArr] = useState();
+  useEffect(()=>{
+    fetch('https://dummyjson.com/products')
+    .then(response=>response.json())
+    .then(data=>setArr(data.products))
+    .catch(err=>console.log(err))
+},[])
   return (
     <div>
-        {/* Do not remove the main div */}
+       <pre>
+        {JSON.stringify(arr)}
+       </pre>
     </div>
   )
 }
